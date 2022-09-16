18a6046d3a45191caf661fbcdb6f4174cebd97a7
vfurmane <vfurmane@student.42.fr>
2022-09-16 15:47:13 +0200

    feat: allow message handler to return -1 to close the connection

--- src/handlers.cpp
+++ src/handlers.cpp
@@ -2,12 +2,18 @@
 
 #include <stdio.h> // TODO Remove le header
 
-void	handleTCPMessage(epoll_event *event)
+int	handleTCPMessage(epoll_event *event)
 {
 	int		bytes_read;
 	char	buffer[MAX_READ + 1];
 	
 	bytes_read = recv(event->data.fd, buffer, MAX_READ, 0);
-	buffer[bytes_read] = '\0';
-	printf("%s", buffer); // DEBUG
+	if (bytes_read == 0)
+		return -1;
+	else
+	{
+		buffer[bytes_read] = '\0';
+		printf("%s", buffer); // DEBUG
+	}
+	return 0;
 }
