55c54ffbcbb81e5c873cfbebace71e8995a86efd
tvogel <tvogel@student.42.fr>
2022-09-19 15:28:38 +0200

    refactor: check if recv return value is less than or equal to 0

--- src/handlers.cpp
+++ src/handlers.cpp
@@ -12,7 +12,7 @@ int	handleTCPMessage(epoll_event *event)
 	char	buffer[MAX_READ + 1];
 	
 	bytes_read = recv(event->data.fd, buffer, MAX_READ, 0);
-	if (bytes_read == 0)
+	if (bytes_read <= 0)
 		return -1;
 	else
 	{
