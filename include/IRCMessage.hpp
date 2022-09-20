8a5f0bb6c53f1e043154dc459cc2e8a9ed3148bb
Pierre Piques <77271282+ppiques@users.noreply.github.com>
2022-09-20 17:09:16 +0200

    feat: add an input parser (#17)
    * feat: added a lexer() function for the input
    
    * refactor: rename and split lexer() function
    
    * fix: fix a segfault in lexer() and added protections on iterator
    
    * fix: fix an issue when lexing without args
    
    * fix: fix an issue when there is no command in input for lexer
    
    * feat: added tests for lexer()
    
    * test: add extra tests
    
    * refactor: changed IRCMessageStream class name to IRCMessage, and changed its' _argCount attribute from int to size_t
    
    * refactor: cleaned up parseArguments() code
    
    * fix: remove useless include inside of handlers.hpp and prefixed parseArguments() private method with  an '_'
    
    Co-authored-by: Pierre PIQUES <ppiques@e1r3p2.clusters.42paris.fr>
    Co-authored-by: Pierre PIQUES <ppiques@e2r2p4.clusters.42paris.fr>

---
+++ include/IRCMessage.hpp
@@ -0,0 +1,25 @@
+#ifndef IRCMESSAGE_HPP
+# define IRCMESSAGE_HPP
+
+#include <string>
+
+class IRCMessage
+{
+	public:
+
+	IRCMessage(const std::string &input);
+	
+	void	parse();
+
+	private:
+
+	void	_parseArguments(std::string::const_iterator &it);
+
+	std::string		_input;
+	std::string		*_prefix;
+	std::string		_command;
+	std::string		_arguments[15];
+	size_t			_argCount;
+};
+
+#endif
