ffdfe9ac3322eb0705806667e949348119fcd9f9
tvogel <tvogel@student.42.fr>
2022-09-22 12:02:56 +0200

    refactor: cleaner execute function

--- src/executeCMD.cpp
+++ src/executeCMD.cpp
@@ -1,27 +1,16 @@
 #include "../include/executeCMD.hpp"
 #include <iostream>
 
-void	execute_cmd(std::string command_to_execute)
+void	executeCommand(IRCMessage &message)
 {
-	std::string		cmd_list[4];
-	typedef void	(*cmdToExec)(void);
+	std::string		commands_name[] = {};
+	typedef void	(*t_command)(void);
 
-	cmdToExec CmdPointer[4] = {
-		// &command_connect,
-		// &command_nick,
-		// &command_join,
-		// &command_query
-	};
-
-	cmd_list[0] = "CONNECT";
-	cmd_list[1] = "NICK";
-	cmd_list[2] = "JOIN";
-	cmd_list[3] = "QUERY";
+	t_command commands[] = {};
 
 	for (int i = 0; i < 4; i++) {
-		if (cmd_list[i] == command_to_execute) {
-			CmdPointer[i]();
+		if (commands_name[i] == message.getCommand()) {
+			commands[i]();
 		}
 	}
 }
-
