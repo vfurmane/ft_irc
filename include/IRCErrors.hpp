6a0f45c1de7ab5539ffeaa745d20f56ce7627a52
vfurmane <vfurmane@student.42.fr>
2022-09-27 16:39:50 +0200

    fix: change the ERR_UNKNOWNCOMMAND code

--- include/IRCErrors.hpp
+++ include/IRCErrors.hpp
@@ -10,7 +10,7 @@ class ERR_UNKNOWNCOMMAND : public std::exception
 		const std::string	_str;
 
 	public:
-		static const int code = 431;
+		static const int code = 421;
 		ERR_UNKNOWNCOMMAND(const std::string &command) : _str(command + " :Unknown command") {}
 		~ERR_UNKNOWNCOMMAND(void) throw() {}
 		virtual const char* what() const throw()
