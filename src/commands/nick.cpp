e2acbcd5c3d242dbba3b246146d3fb1c6f0c32c6
vfurmane <vfurmane@student.42.fr>
2022-09-29 16:58:31 +0200

    feat: parse the nickname in NICK command

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -1,9 +1,46 @@
 #include "commands.hpp"
 
-int	command_nick(Message &message, Dependencies &deps)
+static bool isLetter(char c)
+{
+	if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z'))
+		return false;
+	return true;
+}
+
+static bool isDigit(char c)
+{
+	if (c < '0' || c > '9')
+		return false;
+	return true;
+}
+
+static bool isSpecialCharacter(char c)
+{
+	static const std::string special_chars = "[]\\`_^{|}";
+
+	return special_chars.find(c) != std::string::npos;
+}
+
+static bool	isValidNickname(const std::string &nick)
+{
+	if (nick.empty() || nick.length() > 9)
+		return false;
+	if (!isLetter(nick[0]) && !isSpecialCharacter(nick[0]))
+		return false;
+	for (std::string::const_iterator it = nick.begin(); it < nick.end(); ++it)
+	{
+		if (!isLetter(*it) && !isDigit(*it) && !isSpecialCharacter(*it) && *it != '-')
+			return false;
+	}
+	return true;
+}
+
+int		command_nick(Message &message, Dependencies &deps)
 {
 	if (message.argCount < 1 || message.arguments[0].empty())
 		throw ERR_NONICKNAMEGIVEN();
+	if (!isValidNickname(message.arguments[0]))
+		return 1;
 	message.peer.setNickname(message.arguments[0]);
 	for (PeerManager::const_iterator it = deps.peers.begin(); it != deps.peers.end(); ++it)
 		it->second.sendMessage(NickMessage(message.peer, message.arguments[0]));
--- test/commands/nick.test.cpp
+++ test/commands/nick.test.cpp
@@ -14,6 +14,47 @@
 #include "../src/PeerManager.cpp"
 #include "../src/Server.cpp"
 
+TEST_CASE("isValidNickname")
+{
+	SECTION("when nick is empty")
+	{
+		REQUIRE( !isValidNickname("") );
+	};
+	SECTION("when nick is too long")
+	{
+		REQUIRE( !isValidNickname("johndoe123456") );
+	};
+	SECTION("when nick starts with a digit")
+	{
+		REQUIRE( !isValidNickname("123john") );
+		REQUIRE( !isValidNickname("672jade") );
+	};
+	SECTION("when nick starts with a forbidden character")
+	{
+		REQUIRE( !isValidNickname("-john") );
+		REQUIRE( !isValidNickname("#john") );
+		REQUIRE( !isValidNickname("@john") );
+	};
+	SECTION("when nick contains a forbidden character")
+	{
+		REQUIRE( !isValidNickname("john#") );
+		REQUIRE( !isValidNickname("j@de") );
+	};
+	SECTION("when nick contains multiple forbidden characters")
+	{
+		REQUIRE( !isValidNickname("j%hn#") );
+		REQUIRE( !isValidNickname("j@de@hi") );
+	};
+	SECTION("when nick is valid")
+	{
+		REQUIRE( isValidNickname("john-doe1") );
+		REQUIRE( isValidNickname("j4de") );
+		REQUIRE( isValidNickname("[john]") );
+		REQUIRE( isValidNickname("[{jade}]") );
+		REQUIRE( isValidNickname("[{-}]") );
+		REQUIRE( isValidNickname("[0-1]") );
+	};
+};
 
 TEST_CASE("NICK")
 {
