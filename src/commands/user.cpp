45ade6a9c9606345e824f9b1ec81e80b2093bab7
Pierre Piques <77271282+ppiques@users.noreply.github.com>
2022-09-30 11:53:20 +0200

    feat: add extra parsing for user command and tests (#29)
    * fix: add additional check for USER parsing
    
    * refactor: add mocks.hpp to allow for easier commands testing
    
    * fix: tab instead of space and std::string to static const std::string in user.cpp

--- src/commands/user.cpp
+++ src/commands/user.cpp
@@ -1,6 +1,18 @@
 #include "commands.hpp"
 #include "IRCErrors.hpp"
 
+static bool	forbidden_user_char(const std::string &user)
+{
+	static const std::string forbidden = "@\r\n ";
+
+	for (std::string::const_iterator it = user.begin(); it != user.end(); ++it)
+	{
+		if (forbidden.find(*it) != std::string::npos || (*it) == '\0')
+			return (true);
+	}
+	return (false);
+}
+
 int	command_user(Message &message, Dependencies &deps)
 {
 	(void)deps;
@@ -10,6 +22,8 @@ int	command_user(Message &message, Dependencies &deps)
 		  throw ERR_ALREADYREGISTERED();
 	if (message.arguments[1].length() != 1)
 		throw ERR_UMODEUNKNOWNFLAG(); // DELETE
+	if (forbidden_user_char(message.arguments[0]) == true)
+		return (1);
 	message.peer.registration(message.arguments[0], message.arguments[1], message.arguments[3]);
-	return 1;
+	return (1);
 }
