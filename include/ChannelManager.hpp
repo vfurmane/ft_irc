3e7628d68c56b403119d6809606d780de9ca7838
Théo Vogel <45829081+theovgl@users.noreply.github.com>
2022-10-04 15:01:55 +0200

    feat: add ChannelManager class (#41)
    * feat: create ChannelManager class
    
    * refactor: include Channel.hpp
    
    * test: add an empty test file
    
    * fix: operator [] return a Channel instead of a ChannelManarger
    
    * fix: add ChannelManager.test.cpp in Makefile

---
+++ include/ChannelManager.hpp
@@ -0,0 +1,32 @@
+#ifndef CHANNELMANAGER_HPP
+# define CHANNELMANAGER_HPP
+
+# include "Channel.hpp"
+# include <map>
+# include <string>
+
+class ChannelManager
+{
+	public:
+		typedef std::map<std::string, Channel>::iterator		iterator;
+		typedef std::map<std::string, Channel>::const_iterator	const_iterator;
+		
+		ChannelManager();
+		ChannelManager(const ChannelManager &obj);
+		ChannelManager &operator=(const ChannelManager &rhs);
+		~ChannelManager();
+
+		Channel &operator[](std::string name);
+			
+		iterator		begin(void);
+		iterator		end(void);
+		const_iterator	begin(void) const;
+		const_iterator	end(void) const;
+		std::pair<ChannelManager::iterator, bool>	add(std::string name);
+		void			remove(std::string name);
+	
+	private:
+		std::map<std::string, Channel>	_channels;	
+};
+
+#endif
---
+++ src/ChannelManager.cpp
@@ -0,0 +1,52 @@
+#include "ChannelManager.hpp"
+
+ChannelManager::ChannelManager(): _channels()
+{
+
+}
+
+ChannelManager::ChannelManager(const ChannelManager &obj)
+{
+	this->_channels = obj._channels;	
+}
+
+ChannelManager &ChannelManager::operator=(const ChannelManager &rhs)
+{
+	this->_channels = rhs._channels;
+	return *this;
+}
+
+Channel &ChannelManager::operator[](std::string name)
+{
+	return this->_channels.at(name);
+}
+
+ChannelManager::iterator ChannelManager::begin()
+{
+	return this->_channels.begin();
+}
+
+ChannelManager::iterator ChannelManager::end()
+{
+	return this->_channels.end();
+}
+
+ChannelManager::const_iterator ChannelManager::begin() const
+{
+	return this->_channels.begin();
+}
+
+ChannelManager::const_iterator ChannelManager::end() const
+{
+	return this->_channels.end();
+}
+
+std::pair<ChannelManager::iterator, bool> ChannelManager::add(std::string name)
+{
+	return this->_channels.insert(std::make_pair(name, Channel(name)));
+}
+
+void ChannelManager::remove(std::string name)
+{
+	this->_channels.erase(name);
+}
