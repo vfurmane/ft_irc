a36d02af541530627272841a712231d23093739b
tvogel <tvogel@student.42.fr>
2022-10-04 16:36:02 +0200

    refactor: optimize quit message

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -2,10 +2,7 @@
 
 int	command_quit(Message &message, Dependencies &deps)
 {
-	std::string	msg;
-
-	msg = "Quit: " + message.arguments[0];
-	message.peer.sendMessage(ErrorMessage(message.peer, msg));
+	message.peer.sendMessage(ErrorMessage(message.peer, "Quit: " + message.arguments[0]));
 	deps.peers.closeConnection(message.peer.getFd());
 	return 0;
 }
