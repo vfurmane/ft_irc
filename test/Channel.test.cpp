9515764a0af2ecc5bf7eb36f0a9bbca7f9ec797b
vfurmane <vfurmane@student.42.fr>
2022-10-05 12:08:32 +0200

    feat: add a key attribute in Channel

--- test/Channel.test.cpp
+++ test/Channel.test.cpp
@@ -1,3 +1,59 @@
+#include <sstream>
 #include "catch.hpp"
 
+#define private public
 #include "../src/Channel.cpp"
+
+TEST_CASE("Channel::setKey")
+{
+	SECTION("should save the key")
+	{
+		Channel	channel("general");
+
+		channel.setKey("key");
+		REQUIRE( channel._key == "key" );
+	};
+};
+
+TEST_CASE("Channel::unsetKey")
+{
+	SECTION("should change the key")
+	{
+		Channel	channel("general");
+
+		channel._key = "key";
+		channel.unsetKey();
+		REQUIRE( channel._key.empty() );
+	};
+};
+
+TEST_CASE("Channel::setKey and Channel::unsetKey")
+{
+	SECTION("should change the key")
+	{
+		Channel	channel("general");
+
+		channel.setKey("key");
+		channel.unsetKey();
+		channel.setKey("password");
+		REQUIRE( channel._key == "password" );
+	};
+};
+
+TEST_CASE("Channel::compareKey")
+{
+	SECTION("should validate the key")
+	{
+		Channel	channel("general");
+
+		channel._key = "key";
+		REQUIRE( channel.compareKey("key") );
+	};
+	SECTION("should not validate the key")
+	{
+		Channel	channel("general");
+
+		channel._key = "key";
+		REQUIRE( !channel.compareKey("password") );
+	};
+};
