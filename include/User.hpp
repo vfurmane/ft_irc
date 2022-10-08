f3dbbd792396a1038d785101b4799ac42d3d8c8d
vfurmane <vfurmane@student.42.fr>
2022-10-08 18:13:44 +0200

    feat: add a Channel reference to User

--- include/User.hpp
+++ include/User.hpp
@@ -4,15 +4,20 @@
 # include <string>
 # include "Peer.hpp"
 
+class Channel;
+
 class User
 {
 	public:
-		User(Peer &peer);
+		User(Peer &peer, Channel &channel);
 		User(const User &obj);
 		User &operator=(const User &rhs);
 		~User(void);
 
 		Peer	&peer;
+		Channel	&channel;
 };
+
+# include "Channel.hpp"
 
 #endif
