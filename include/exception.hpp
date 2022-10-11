004b7bcc399900ab491088fe6ae308ff877d429d
Valentin <76964081+vfurmane@users.noreply.github.com>
2022-10-11 17:43:08 +0200

    refactor: make sysClassError a class (#82)

--- include/exception.hpp
+++ include/exception.hpp
@@ -4,14 +4,17 @@
 #include <exception>
 #include <string>
 
-struct sysCallError : public std::exception {
-	std::string _str;
-	sysCallError(const std::string &syscall, const std::string &str) : _str(syscall + ": " + str) {}
-	~sysCallError(void) throw() {}
-	virtual const char* what() const throw()
-	{
-		return this->_str.c_str();
-	}
+class sysCallError : public std::exception {
+	private:
+		std::string _str;
+
+	public:
+		sysCallError(const std::string &syscall, const std::string &str) : _str(syscall + ": " + str) {}
+		~sysCallError(void) throw() {}
+		virtual const char* what() const throw()
+		{
+			return this->_str.c_str();
+		}
 };
 
 #endif
