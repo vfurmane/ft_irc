a705f3a64d918bc75811d4816d59a2d96919d69d
tvogel <tvogel@student.42.fr>
2022-10-11 17:55:52 +0200

    test: add a test with complex name

--- test/commands/part.test.cpp
+++ test/commands/part.test.cpp
@@ -27,6 +27,25 @@ TEST_CASE("PART")
 		message.argCount = 1;
 		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOSUCHCHANNEL );
 	}
+	SECTION("should work with complex channel name #1")
+	{
+		Channel channel("channel");
+		peer._user = "test_user";
+		peer.createChannel(channel);
+		message.arguments[0] = "#Channel";
+		message.argCount = 1;
+		REQUIRE_NOTHROW( command_part(message, deps) );
+	}
+	SECTION("should remove the user from channel")
+	{
+		Channel channel("channel");
+		peer._user = "test_user";
+		peer.createChannel(channel);
+		message.arguments[0] = "channel";
+		message.argCount = 1;
+		REQUIRE_NOTHROW( command_part(message, deps) );
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
+	}
 	SECTION("user not on channel")
 	{
 		Channel channel("channel");
@@ -45,5 +64,29 @@ TEST_CASE("PART")
 		message.arguments[0] = "channel";
 		message.argCount = 1;
 		REQUIRE_NOTHROW( command_part(message, deps) );
+	}
+	SECTION("should part from a list of channel")
+	{
+		Channel channel1("channel1");
+		Channel channel2("channel2");
+		Channel channel3("channel3");
+		Channel channel4("channel4");
+		peer._user = "test_user";
+		peer.createChannel(channel1);
+		peer.createChannel(channel2);
+		peer.createChannel(channel3);
+		peer.createChannel(channel4);
+		message.arguments[0] = "channel1,channel2,channel4,channel3";
+		message.argCount = 1;
+		REQUIRE_NOTHROW( command_part(message, deps) );
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
+		message.arguments[0] = "channel1";
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
+		message.arguments[0] = "channel2";
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
+		message.arguments[0] = "channel3";
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
+		message.arguments[0] = "channel4";
+		REQUIRE_THROWS_AS( command_part(message, deps), ERR_NOTONCHANNEL );
 	}
 }
