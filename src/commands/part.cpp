902b2f50972806983e76678a809a3b25b415ddd3
tvogel <tvogel@student.42.fr>
2022-10-12 11:52:01 +0200

    feat: part command reply with the right message

--- src/commands/part.cpp
+++ src/commands/part.cpp
@@ -11,22 +11,39 @@ int command_part(Message &message, Dependencies &deps)
 
 	std::vector<std::string> channels = parseList(message.arguments[0]);
 	std::vector<std::string>::const_iterator channel_it = channels.begin();
-	
+
 	ChannelManager &channel_manager = deps.channels;
-	
+
 	while (channel_it != channels.end())
 	{
-		if (channel_manager.has(*channel_it) == false)
+		_base_channel base_channel = Channel::parse(*channel_it);
+		if (channel_manager.has(base_channel.getName()) == false)
 		{
+			message.peer.sendMessage(ERR_NOSUCHCHANNEL(*channel_it));
 			throw ERR_NOSUCHCHANNEL(*channel_it);
 		}
 		else 
 		{
-			Channel &channel = deps.channels[*channel_it];
+			Channel &channel = deps.channels[base_channel.getName()];
 			if (channel.users.has(message.peer.getUsername()) == true)
+			{
 				channel.remove(channel.users[message.peer.getUsername()]);
+				if (message.arguments[1].empty())
+				{
+					Message reply(message.peer, message.peer.getNickname());
+					channel.sendMessage(reply);
+				}
+				else
+				{
+					Message reply(message.peer, message.arguments[1]);
+					channel.sendMessage(reply);
+				}
+			}
 			else
-				throw ERR_NOTONCHANNEL(*channel_it);
+			{
+				message.peer.sendMessage(ERR_NOTONCHANNEL(*channel_it));
+				throw ERR_NOTONCHANNEL(*channel_it);	
+			}
 		}
 		++channel_it;
 	}
