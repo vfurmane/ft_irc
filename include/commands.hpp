a451f87e6610264e736221d6f787f9fc0176bd29
Valentin <76964081+vfurmane@users.noreply.github.com>
2022-10-12 10:36:18 +0200

    refactor: move Dependencies structure to a correct place (#83)

--- include/commands.hpp
+++ include/commands.hpp
@@ -14,16 +14,16 @@ struct ErrorMessage : public Message
 	ErrorMessage(Peer &peer, const std::string &message, bool include_prefix = false);
 };
 
+struct NickMessage : public Message
+{
+	NickMessage(Peer &peer, const std::string &new_nick, bool include_prefix = false);
+};
+
 struct Dependencies
 {
 	Configuration	&config;
 	PeerManager	&peers;
 	ChannelManager	&channels;
-};
-
-struct NickMessage : public Message
-{
-	NickMessage(Peer &peer, const std::string &new_nick, bool include_prefix = false);
 };
 
 int	command_nick(Message &message, Dependencies &deps);
