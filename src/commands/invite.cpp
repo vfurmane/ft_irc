6c7650a8ce255318c864a07317fd665e6f6f666e
tvogel <tvogel@student.42.fr>
2022-10-17 17:46:47 +0200

    feat: add invite command

---
+++ src/commands/invite.cpp
@@ -0,0 +1,50 @@
+#include "commands.hpp"
+
+// INVITE <nickname> <channel>
+
+static void	add_invitation(Message &message, Channel &target_channel)
+{
+	if (target_channel.users.hasByNickname(message.arguments[0]))
+		throw ERR_USERONCHANNEL(message.arguments[0], message.arguments[1]);
+	else
+	{
+		target_channel.addInvitation(message.arguments[0]);
+		message.peer.sendMessage(RPL_INVITING(message.peer, message.arguments[1], message.arguments[0], false));
+	}
+}
+
+int	command_invite(Message &message, Dependencies &deps)
+{
+	if (message.argCount < 2)
+		throw ERR_NEEDMOREPARAMS("INVITE");
+
+	if (!_base_channel::isValidName(message.arguments[1]))
+		return 1;
+	_base_channel base_channel = _base_channel::parse(message.arguments[1]);
+	if (deps.channels.has(base_channel))
+	{
+		if (!deps.peers.hasByNickname(message.arguments[0]))
+		{
+			message.peer.sendMessage(ERR_NOSUCHNICK(message.arguments[0]));
+			return 1;
+		}
+
+		Channel target_channel(deps.channels.get(base_channel));
+
+		if (!target_channel.users.hasByNickname(message.peer.getNickname()))
+			message.peer.sendMessage(ERR_NOTONCHANNEL(message.arguments[1]));
+		else
+		{
+			if ((target_channel.getFlags() & FLAG_INVITE) == FLAG_INVITE)
+			{
+				if (target_channel.users.getByNickname(message.peer.getNickname()).getStatus() > 0)
+					add_invitation(message, target_channel);
+				else
+					message.peer.sendMessage(ERR_CHANOPRIVSNEEDED(message.arguments[1]));
+			}
+			else
+				add_invitation(message, target_channel);
+		}
+	}
+	return 1;
+}
