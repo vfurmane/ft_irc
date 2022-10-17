5e06fcd5537dc9da53b0f1becbdbf2139c0dd6aa
vfurmane <vfurmane@student.42.fr>
2022-10-17 11:34:22 +0200

    fix: use an in use nickname

--- src/IRCReplies.cpp
+++ src/IRCReplies.cpp
@@ -3,7 +3,7 @@
 RPL_WELCOME::RPL_WELCOME(Peer &peer, bool include_prefix) : Message(peer, std::string())
 {
 	this->command = "001";
-	this->arguments[0] = peer.getUsername();
+	this->arguments[0] = peer.getNickname();
 	this->arguments[1] = "Welcome to the " NETWORK_NAME " Network " + peer.generatePrefix();
 	this->argCount = 2;
 	if (include_prefix)
@@ -14,7 +14,7 @@ RPL_WELCOME::RPL_WELCOME(Peer &peer, bool include_prefix) : Message(peer, std::s
 RPL_YOURHOST::RPL_YOURHOST(Peer &peer, bool include_prefix) : Message(peer, std::string())
 {
 	this->command = "002";
-	this->arguments[0] = peer.getUsername();
+	this->arguments[0] = peer.getNickname();
 	this->arguments[1] = "Your host is " SERVER_NAME ", running version " SERVER_VERSION;
 	this->argCount = 2;
 	if (include_prefix)
@@ -25,7 +25,7 @@ RPL_YOURHOST::RPL_YOURHOST(Peer &peer, bool include_prefix) : Message(peer, std:
 RPL_CREATED::RPL_CREATED(Peer &peer, bool include_prefix) : Message(peer, std::string())
 {
 	this->command = "003";
-	this->arguments[0] = peer.getUsername();
+	this->arguments[0] = peer.getNickname();
 	this->arguments[1] = "This server was created " SERVER_CREATION_DATE;
 	this->argCount = 2;
 	if (include_prefix)
@@ -36,7 +36,7 @@ RPL_CREATED::RPL_CREATED(Peer &peer, bool include_prefix) : Message(peer, std::s
 RPL_MYINFO::RPL_MYINFO(Peer &peer, bool include_prefix) : Message(peer, std::string())
 {
 	this->command = "004";
-	this->arguments[0] = peer.getUsername();
+	this->arguments[0] = peer.getNickname();
 	this->arguments[1] = "" SERVER_NAME " " SERVER_VERSION " " AVAILABLE_USER_MODES " " AVAILABLE_CHANNEL_MODES;
 	this->argCount = 2;
 	if (include_prefix)
