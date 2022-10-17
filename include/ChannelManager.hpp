417aed8e33e6c172d7a549e435bd61753e1cc606
Valentin <76964081+vfurmane@users.noreply.github.com>
2022-10-17 11:33:54 +0200

    feat: add getters to ChannelManager (#102)

--- include/ChannelManager.hpp
+++ include/ChannelManager.hpp
@@ -21,6 +21,8 @@ class ChannelManager : public Manager<std::string, Channel>
 		Channel			&add(const std::string &name);
 		Channel			&add(const _base_channel &base_channel);
 		void			remove(const std::string &name);
+		Channel			&get(const std::string &name);
+		Channel			&get(const _base_channel &base_channel);
 		bool			has(const std::string &name) const;
 		bool			has(const _base_channel &base_channel) const;
 };
