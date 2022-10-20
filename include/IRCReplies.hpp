6ad4aebe56a273c265d72d09f3cbf08f248e8cd8
Valentin <76964081+vfurmane@users.noreply.github.com>
2022-10-20 17:30:55 +0200

    Move configuration defines inside the Configuration class (#134)
    * fix: prevent user from joining a KEY_FLAG channel without inputing a key
    
    * fix: INVITE message invites the right target
    
    * refactor: move configuration defines inside the Configuration class
    
    Co-authored-by: ppiques <ppiques@student.42.fr>

--- include/IRCReplies.hpp
+++ include/IRCReplies.hpp
@@ -4,13 +4,6 @@
 # include "Message.hpp"
 class Peer;
 
-# define NETWORK_NAME "gibuffet"
-# define SERVER_NAME "localhost" // TODO set real hostname
-# define SERVER_VERSION "dev"
-# define SERVER_CREATION_DATE "00:00:00 Jan 01 1970" // TODO set real date
-# define AVAILABLE_USER_MODES "i" // TODO use this with MODE
-# define AVAILABLE_CHANNEL_MODES "Oik" // TODO use this with MODE
-
 struct RPL_WELCOME : public Message
 {
 	RPL_WELCOME(Peer &peer, bool include_prefix = false);
