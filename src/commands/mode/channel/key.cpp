301948098df62f1d01544bc157ef5baa1252193f
vfurmane <vfurmane@student.42.fr>
2022-10-20 15:39:05 +0200

    fix: send the stringified channel instead of just the name

--- src/commands/mode/channel/key.cpp
+++ src/commands/mode/channel/key.cpp
@@ -3,7 +3,7 @@
 void	flag_key(bool add_flag, User &author, Channel &channel, const std::string &argument)
 {
 	if (author.getStatus() == CHANNEL_USER)
-		throw ERR_CHANOPRIVSNEEDED(channel.getName());
+		throw ERR_CHANOPRIVSNEEDED(channel.stringify());
 	if (add_flag == true)
 	{
 		channel.setKey(argument);
